b9a43154259bb9e1b0c39d97be96346cf83b0651
Yohann THEPAUT <contact@ythepaut.com>
2020-10-29 13:42:09 +0100

    Première version edition temps réel

--- server.js
+++ server.js
@@ -1,22 +1,57 @@
-let express = require("express");
-let app = express();
+let http = require("http"),
+    express = require("express"),
+    path = require("path"),
+    socketio = require("socket.io");
+let app = express(),
+    server = http.createServer(app),
+    io = socketio(server);
 
 
 // setting view engine
 app.set("view engine", "ejs");
 
 
-// setting middlewares
-app.use(express.static("static"));
+// setting static folder
+app.use(express.static(path.join(__dirname, "static")));
 
 
 // creating routes
-app.get("/", ((req, res) => {
+app.get("/", (req, res) => {
     res.render("index");
-}));
+});
+
+
+// TODO remove temp var
+let collabContent = {
+    text : ""
+}
+
+
+// creating socket.io listeners
+io.on("connection", (socket) => {
+
+    // debug messages
+    socket.emit("debug", "Connected with id " + socket.id + ".");
+    socket.broadcast.emit("debug", "User " + socket.id + " joined.");
+    console.log("User " + socket.id + " joined.");
+
+    // sending collab content
+    socket.emit("sync", collabContent);
+
+    // handle collab update
+    socket.on("update", (data) => {
+        collabContent.text = data.text
+        io.sockets.emit("update", data);
+    })
+
+    io.on("disconnect", () => {
+        io.emit("debug", "User " + socket.id + " left.");
+        console.log("User " + socket.id + " left.");
+    });
+});
 
 
 // starting server
-app.listen(8000, () => {
+server.listen(process.env.PORT || 8000, () => {
     console.log("Server is now listening...");
 });
--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,20 @@
+let editor;
+let textAreaEditor;
+let socket;
+
+// TODO remove temp vars
+let lock = false;
+let collabContent = {
+    text : ""
+}
+
+
 // codemirror setup
 window.addEventListener("load", () => {
 
-    let editor = CodeMirror.fromTextArea(document.querySelector(".editor > textarea"), {
+    textAreaEditor = document.querySelector(".editor > textarea");
+
+    editor = CodeMirror.fromTextArea(textAreaEditor, {
         lineNumbers: true,
         viewportMargin: Infinity,
         theme: "dracula",
@@ -26,7 +39,40 @@ window.addEventListener("load", () => {
             cm.replaceSelection(spaces, "end", "+input");
         }
     });
+
+
+
+    // socket io
+
+    socket = io();
+
+
+    // editor listener
+    editor.on("change", () => {
+        if (!lock) {
+            socket.emit("update", {text : editor.getValue()});
+        }
+    });
+    //TODO correct cursor column
+
+    // debug message
+    socket.on("debug", (message) => {
+        console.log(message);
+    });
+
+    // new change received from collaborator
+    socket.on("update", (data) => {
+        collabContent.text = data.text;
+        lock = true;
+        editor.setValue(data.text);
+        lock = false;
+    });
+
+    // sync on connect
+    socket.on("sync", (data) => {
+        collabContent.text = data.text;
+        editor.setValue(data.text);
+    });
+
 });
 
-
-// TODO socket io communication
