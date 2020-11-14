91e6794a1b7defaa2a111b50c34f816b15483b13
Yohann THEPAUT <contact@ythepaut.com>
2020-11-14 14:19:23 +0100

    Deplacement de ./ vers ./src

---
+++ src/models/project.js
@@ -0,0 +1,19 @@
+/**
+ * Project class
+ *
+ * @author      Yohann THEPAUT (ythepaut) <contact@ythepaut.com>
+ * @license     GNU GPL v3.0
+ */
+class Project {
+
+    /**
+     * Project constructor
+     *
+     * @param id            string          Unique project ID
+     * @param owner         int             User ID of project owner, -1 if anonymous
+     */
+    constructor(id, owner, createdAt, editedAt) {
+    }
+
+
+}
